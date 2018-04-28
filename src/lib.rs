a6410a7b37ba7e1719ee16c02c121a9f9faa139e
TheZoq2 <frans.skarman@gmail.com>
2018-04-28 17:43:01 +0200

    Add stopwatch trait

--- src/lib.rs
+++ src/lib.rs
@@ -16,3 +16,20 @@ pub struct Second(pub u32);
 pub struct Millisecond(pub u32);
 #[derive(Clone, Copy)]
 pub struct Microsecond(pub u32);
+
+
+/**
+  A monotonic timer that can be used to measure time passed. It should count from 0 to
+  the max value of TickType and then wrap around.
+*/
+pub trait Stopwatch<TickType, Frequency> {
+    /**
+      Returns the amount of ticks that have passed since the last wraparound.
+    */
+    fn ticks_passed(&self) -> TickType;
+    /**
+      Returns the frequency of the timer. The total elapsed time should be
+      ticks_passed * 1/frequency
+    */
+    fn frequency(&self) -> Frequency;
+}
